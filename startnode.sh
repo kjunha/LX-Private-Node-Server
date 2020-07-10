502279f4b5a8a5b7bf2e5dc33085415d3f37ee6a
kjunha <kjunha77@gmail.com>
2020-07-10 14:36:35 +0900

    add genesis.json

---
+++ startnode.sh
@@ -0,0 +1,1 @@
+geth --networkid 4224 --mine --minerthreads 1 --datadir $(pwd) --nodiscover --rpc --rpcport "8545" --port "30303" --rpccorsdomain "*" --nat "any" --rpcapi eth,web3,personal,net --unlock 0 --password $(pwd)/pass.txt --allow-insecure-unlock --ipcpath $(pwd)/geth.ipc
